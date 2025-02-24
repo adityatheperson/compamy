53f3018d2123ce348fafcf2f9969536dbb2671c1
adityatheperson <44348836+adityatheperson@users.noreply.github.com>
2025-02-24 10:51:44 -0800

    updated prompt

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def grok(imagepath, message):
     completion = client.chat.completions.create(
         model="grok-2-vision-latest",
         messages=messages,
-        temperature=0.01,
+        temperature=0.2,
     )
     return completion.choices[0].message.content
 
@@ -79,7 +79,7 @@ def save_image(e: events.UploadEventArguments):
             f.write(e.content.read())
         ui.notify(f'Uploaded and saved as {e.name}')
     image_path = current_directory + "/" + e.name
-    user_prompt = "what are your remarks about this cartoon character, try to be funny. make sure you do not end on a positive note. Be as sarcastic as you can, please limit it to one sentence and make it spicy. You can be rude because this a nonexistent being and does not have feelings. Try to include stuff included in the picture in the joke. Do not mention that this is a cartoon character. Do not include mentions of the background. Try not to mention the cartoon characters cloths and refer to odd facial features. Try to make it as personal as you can."
+    user_prompt = "roast this person based on their facial features, do not be lighthearted"
     response = grok(image_path, user_prompt)
     output_label.text = response
     print(response)
